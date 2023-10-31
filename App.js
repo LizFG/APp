5deebc710226dbde023437254191bbc9cf4827c9
LizFG <149424807+LizFG@users.noreply.github.com>
2023-10-31 15:59:20 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Liz</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
